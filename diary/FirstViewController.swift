ae94de4c6b2f88ca647a49eef0a963c51258748f
Stephanie Eta <stephbaby@Stephanies-MacBook-Pro-2.local>
2015-02-26 18:48:14 +0000

    Initial Commit

---
+++ diary/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  diary
+//
+//  Created by Stephanie Eta on 26/02/2015.
+//  Copyright (c) 2015 SE. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
